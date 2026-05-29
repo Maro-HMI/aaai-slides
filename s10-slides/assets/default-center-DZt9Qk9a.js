c341b73193df4df0c6843a52b1e5bf215a00171c
Maro <m.a.gomezmaureira@utwente.nl>
2026-05-29 12:15:20 +0200

    Deploy s10-slides — 2026-05-29 12:15

---
+++ s10-slides/assets/default-center-DZt9Qk9a.js
@@ -0,0 +1,1 @@
+import{o as t,b as s,w as a,i as r}from"./modules/vue-D2MlRfyU.js";import{u as o}from"./slidev/context-ChbQBAoa.js";import{_ as n}from"./default-mJqS6EAv.js";const u={__name:"default-center",setup(_){return o(),(e,c)=>(t(),s(n,{class:"ns-default-center"},{default:a(()=>[r(e.$slots,"default")]),_:3}))}};export{u as _};
